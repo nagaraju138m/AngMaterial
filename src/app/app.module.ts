d1d558e65ce41beec0cee4361364218d6e283317
Nagaraju <nagaraju.makanaboyina@peopletech.com>
2022-12-26 18:21:27 +0530

    first material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,42 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RegisterComponent } from './register/register.component';
+import {HttpClientModule} from '@angular/common/http';
+
+// material imports
+import { MatButtonModule } from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatNativeDateModule } from '@angular/material/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { NavbarComponent } from './navbar/navbar.component';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    HttpClientModule,
+    MatNativeDateModule,
+    MatToolbarModule,
+    MatIconModule
   ],
+
+  declarations: [
+    AppComponent,
+    RegisterComponent,
+    NavbarComponent
+  ],
+
+
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/register/register.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl , Validators} from '@angular/forms'
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnInit {
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
